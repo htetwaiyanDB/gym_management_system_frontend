048f4493f7c13cb6b6ce8f1576d272da03e4a1ed
hwy-23db <hwyyy22@gmail.com>
2026-01-18 14:55:58 +0630

    blog post error fix

--- src/App.jsx
+++ src/App.jsx
@@ -27,14 +27,14 @@ const TrainerBookings = lazy (() =>import ("./pages/trainer/TrainerBookings"));
 const TrainerBlogDetails = lazy (() =>import ("./pages/trainer/TrainerBlogDetails"));
 const TrainerSettings = lazy(()=>import("./pages/trainer/TrainerSettings"));
 
-//user
-const UserLayout = lazy(() => import("./layouts/UserLayout"));
-const UserHome = lazy(() => import("./pages/user/UserHome"));
-const UserAttendance = lazy(() => import("./pages/user/UserAttendance"));
-const UserSubscriptions = lazy(() => import("./pages/user/UserSubscriptions"));
-const UserBookings = lazy(() => import("./pages/user/UserBookings"));
-const UserMessages = lazy(() => import("./pages/user/UserMessages"));
-const UserSettings = lazy(() => import("./pages/user/UserSettings"));
+// //user
+// const UserLayout = lazy(() => import("./layouts/UserLayout"));
+// const UserHome = lazy(() => import("./pages/user/UserHome"));
+// const UserAttendance = lazy(() => import("./pages/user/UserAttendance"));
+// const UserSubscriptions = lazy(() => import("./pages/user/UserSubscriptions"));
+// const UserBookings = lazy(() => import("./pages/user/UserBookings"));
+// const UserMessages = lazy(() => import("./pages/user/UserMessages"));
+// const UserSettings = lazy(() => import("./pages/user/UserSettings"));
 
 
 function getToken() {
@@ -107,13 +107,13 @@ export default function App() {
           }
         />
 
-        <Route index element={<UserHome />} />
+        {/* <Route index element={<UserHome />} />
           <Route path="home" element={<UserHome />} />
           <Route path="attendance" element={<UserAttendance />} />
           <Route path="subscriptions" element={<UserSubscriptions />} />
           <Route path="bookings" element={<UserBookings />} />
           <Route path="messages" element={<UserMessages />} />
-          <Route path="settings" element={<UserSettings />} />
+          <Route path="settings" element={<UserSettings />} /> */}
 
 
         {/* Trainer (mobile page) */}
