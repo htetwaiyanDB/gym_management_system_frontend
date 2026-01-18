184ef13a4408713e4fe0fe0cd4442e91077e9b4a
hwy-23db <hwyyy22@gmail.com>
2026-01-18 14:50:30 +0630

    fix blog post error

--- src/pages/user/UserHome.jsx
+++ src/pages/user/UserHome.jsx
@@ -1,7 +1,7 @@
 export default function UserHome() {
   return (
     <div>
-      <h2 style={{ marginBottom: 8 }}>User Dashboard</h2>
+      <h2 style={{ marginBottom: 8 }}>Welcome To Unity GYM</h2>
       <p style={{ opacity: 0.85, marginBottom: 0 }}>
         Welcome! Use the bottom menu to navigate.
       </p>
